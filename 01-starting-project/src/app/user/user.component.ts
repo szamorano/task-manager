d44aaed07b62bb305e2e4ae281433330a167fa6b
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-07-21 18:41:33 -0400

    Use class bindings for dynamic CSS styling

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -11,6 +11,7 @@ import { type User } from './user.model';
 })
 export class UserComponent {
 @Input({required:true}) user!: User;
+@Input({required:true}) selected!: boolean;
 @Output() select = new EventEmitter();
 // select = output<string>();
 
