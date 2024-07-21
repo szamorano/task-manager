1dbc331ff88ccedd5bda2ad464d34724b471326d
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-07-21 16:35:36 -0400

    Add tasks component and get selected user

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 
 
 @Component({
@@ -13,6 +13,7 @@ export class UserComponent {
 @Input({required:true}) avatar!: string;
 @Input({required:true}) name!: string;
 @Output() select = new EventEmitter();
+// select = output<string>();
 
 get imagePath()
 {
